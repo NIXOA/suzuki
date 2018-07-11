a31c0c0c4824597893703230ae4b6398cb9b4241
qiaoxin <qiaoxin>
2018-07-11 19:10:47 +0800

    Initial commit

---
+++ eureka-client/main/java/com/suzuki/client/Application.java
@@ -0,0 +1,19 @@
+package com.suzuki.client;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author yekai
+ * @Date 2018/7/11 10:52
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+
+    }
+}
---
+++ eureka-service/main/java/com/suzuki/eureka/service/Application.java
@@ -0,0 +1,17 @@
+package com.suzuki.eureka.service;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author yekai
+ * @Date 2018/7/11 13:24
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
