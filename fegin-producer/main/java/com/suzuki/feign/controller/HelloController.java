0e2fc8872596b70982f3bc13c62a7168468cf641
qiaoxin <qiaoxin>
2018-08-06 14:17:05 +0800

    添加gateway网关

---
+++ fegin-producer/main/java/com/suzuki/feign/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.suzuki.feign.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: xujin
+ **/
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+
+    @GetMapping("")
+    public String hello(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+}
